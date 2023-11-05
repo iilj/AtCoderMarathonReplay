edee00b99b9508a7c9bf7ffe20cfab66a269b658
iilj <u.eeoou@gmail.com>
2023-11-05 21:22:22 +0900

    Add json: Toyota Programming Contest 2023#6（AtCoder Heuristic Contest 026）

--- crawler/lib/ContestListPage.py
+++ crawler/lib/ContestListPage.py
@@ -4,17 +4,19 @@ from bs4.element import Tag
 import re
 from datetime import datetime
 
-from typing_extensions import Literal
+from typing import Literal
 
-SubmissionStatus = Literal['AC', 'WA', 'IE', 'OLE', 'RE', 'TLE', 'MLE', 'CE', 'WJ', 'WR']
+SubmissionStatus = Literal[
+    "AC", "WA", "IE", "OLE", "RE", "TLE", "MLE", "CE", "WJ", "WR"
+]
 
 
 class ContestListPage:
-    """コンテスト一覧ページの1つを表すクラス．コンストラクタ内で HTML をパースする．
-    """
+    """コンテスト一覧ページの1つを表すクラス．コンストラクタ内で HTML をパースする．"""
+
     class Contest:
-        """コンテストインスタンス
-        """
+        """コンテストインスタンス"""
+
         time: datetime
         time_unix: int
         contest_slug: str
@@ -22,8 +24,8 @@ class ContestListPage:
         duration_minutes: int
         rated: bool
 
-        contest_href_pattern: Pattern[str] = re.compile(r'/contests/([^/]+)')
-        duration_pattern: Pattern[str] = re.compile(r'(\d+):(\d+)')
+        contest_href_pattern: Pattern[str] = re.compile(r"/contests/([^/]+)")
+        duration_pattern: Pattern[str] = re.compile(r"(\d+):(\d+)")
 
         def __init__(self, table_row: Tag) -> None:
             """コンテスト一覧ページ内のテーブルのある行タグから，コンテストインスタンスを初期化する．
@@ -31,32 +33,38 @@ class ContestListPage:
             Args:
                 table_row (Tag): 行タグ
             """
-            table_data_list: List[Tag] = table_row.select('td')
+            table_data_list: List[Tag] = table_row.select("td")
 
             time_str: str = table_data_list[0].get_text()
-            self.time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S+0900')
+            self.time = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S+0900")
             self.time_unix = int(self.time.timestamp())
 
-            contest_tag: Tag = table_data_list[1].find('a')
-            contest_href_match: Optional[Match[str]] = self.contest_href_pattern.search(contest_tag['href'])
+            contest_tag: Tag = table_data_list[1].find("a")
+            contest_href_match: Optional[Match[str]] = self.contest_href_pattern.search(
+                contest_tag["href"]
+            )
             assert contest_href_match is not None
             self.contest_slug = contest_href_match.group(1)
             self.contest_name = contest_tag.get_text()
 
             duration_str: str = table_data_list[2].get_text()
-            duration_match: Optional[Match[str]] = self.duration_pattern.search(duration_str)
+            duration_match: Optional[Match[str]] = self.duration_pattern.search(
+                duration_str
+            )
             assert duration_match is not None
             hours: int = int(duration_match.group(1))
             minutes: int = int(duration_match.group(2))
             self.duration_minutes = hours * 60 + minutes
 
             rated_range_str: str = table_data_list[3].get_text().strip()
-            self.rated = (rated_range_str == 'All')
+            self.rated = rated_range_str == "All"
 
         def __repr__(self) -> str:
-            return ('<Contest '
-                    f'time={self.time}, slug={self.contest_slug}, name={self.contest_name}, '
-                    f'duration={self.duration_minutes}, rated={self.rated}>')
+            return (
+                "<Contest "
+                f"time={self.time}, slug={self.contest_slug}, name={self.contest_name}, "
+                f"duration={self.duration_minutes}, rated={self.rated}>"
+            )
 
     contests: List[Contest]
 
@@ -68,8 +76,8 @@ class ContestListPage:
         """
         # get submissions
         soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
-        table_rows: List[Tag] = soup.select('div.table-responsive table.table tbody tr')
+        table_rows: List[Tag] = soup.select("div.table-responsive table.table tbody tr")
         self.contests = [ContestListPage.Contest(table_row) for table_row in table_rows]
 
     def __repr__(self) -> str:
-        return (f'<ContestListPage contests={self.contests}>')
+        return f"<ContestListPage contests={self.contests}>"
--- crawler/lib/SubmissionListPage.py
+++ crawler/lib/SubmissionListPage.py
@@ -5,17 +5,19 @@ from urllib.parse import ParseResult, urlparse, parse_qs
 import re
 from datetime import datetime
 
-from typing_extensions import Literal
+from typing import Literal
 
-SubmissionStatus = Literal['AC', 'WA', 'IE', 'OLE', 'RE', 'TLE', 'MLE', 'CE', 'WJ', 'WR']
+SubmissionStatus = Literal[
+    "AC", "WA", "IE", "OLE", "RE", "TLE", "MLE", "CE", "WJ", "WR"
+]
 
 
 class SubmissionListPage:
-    """提出一覧ページの1つを表すクラス．コンストラクタ内で HTML をパースする．
-    """
+    """提出一覧ページの1つを表すクラス．コンストラクタ内で HTML をパースする．"""
+
     class Submission:
-        """提出インスタンス
-        """
+        """提出インスタンス"""
+
         time: datetime
         time_unix: int
         contest: str
@@ -31,9 +33,9 @@ class SubmissionListPage:
         time_consumption: int
         memory_consumption: int
 
-        task_href_pattern: Pattern[str] = re.compile(r'/contests/([^/]+)/tasks/([^/]+)')
-        user_href_pattern: Pattern[str] = re.compile(r'/users/([^/]+)')
-        score_href_pattern: Pattern[str] = re.compile(r'(-?\d+)\.(\d+)')
+        task_href_pattern: Pattern[str] = re.compile(r"/contests/([^/]+)/tasks/([^/]+)")
+        user_href_pattern: Pattern[str] = re.compile(r"/users/([^/]+)")
+        score_href_pattern: Pattern[str] = re.compile(r"(-?\d+)\.(\d+)")
 
         def __init__(self, table_row: Tag) -> None:
             """提出一覧ページ内のテーブルのある行タグから，提出インスタンスを初期化する．
@@ -41,32 +43,40 @@ class SubmissionListPage:
             Args:
                 table_row (Tag): 行タグ
             """
-            table_data_list: List[Tag] = table_row.select('td')
+            table_data_list: List[Tag] = table_row.select("td")
 
             time_str: str = table_data_list[0].get_text()
-            self.time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S+0900')
+            self.time = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S+0900")
             self.time_unix = int(self.time.timestamp())
 
-            task_tag: Tag = table_data_list[1].find('a')
-            task_href_match: Optional[Match[str]] = self.task_href_pattern.search(task_tag['href'])
+            task_tag: Tag = table_data_list[1].find("a")
+            task_href_match: Optional[Match[str]] = self.task_href_pattern.search(
+                task_tag["href"]
+            )
             assert task_href_match is not None
             self.contest = task_href_match.group(1)
             self.task = task_href_match.group(2)
 
-            user_name_tag: Tag = table_data_list[2].find('a')
+            user_name_tag: Tag = table_data_list[2].find("a")
             # self.user_name = user_name_tag.get_text()
-            user_href_match: Optional[Match[str]] = self.user_href_pattern.search(user_name_tag['href'])
+            user_href_match: Optional[Match[str]] = self.user_href_pattern.search(
+                user_name_tag["href"]
+            )
             assert user_href_match is not None
             self.user_name = user_href_match.group(1)
 
-            lnag_tag: Tag = table_data_list[3].find('a')
+            lnag_tag: Tag = table_data_list[3].find("a")
             self.lang_name = lnag_tag.get_text()
-            lang_url_parse_result: ParseResult = urlparse(lnag_tag['href'])
-            lang_url_query_dict: Dict[str, List[str]] = parse_qs(lang_url_parse_result.query)
-            self.lang_id = int(lang_url_query_dict['f.Language'][0])
+            lang_url_parse_result: ParseResult = urlparse(lnag_tag["href"])
+            lang_url_query_dict: Dict[str, List[str]] = parse_qs(
+                lang_url_parse_result.query
+            )
+            self.lang_id = int(lang_url_query_dict["f.Language"][0])
 
             score_str: str = table_data_list[4].get_text()
-            score_href_match: Optional[Match[str]] = self.score_href_pattern.search(score_str)
+            score_href_match: Optional[Match[str]] = self.score_href_pattern.search(
+                score_str
+            )
             if score_href_match is not None:
                 score_fst: int = int(score_href_match.group(1))
                 score_snd_str: str = score_href_match.group(2)
@@ -79,59 +89,68 @@ class SubmissionListPage:
             else:
                 self.score: int = int(table_data_list[4].get_text())
                 self.magnification = 1
-            self.submission_id = int(table_data_list[4]['data-id'])
+            self.submission_id = int(table_data_list[4]["data-id"])
 
             source_length_str: str = table_data_list[5].get_text()
-            self.source_length = int(source_length_str.split(' ')[0])
+            self.source_length = int(source_length_str.split(" ")[0])
 
-            status_span: Tag = table_data_list[6].find('span')
+            status_span: Tag = table_data_list[6].find("span")
             self.status = self.__str_to_status(status_span.get_text())
 
-            if self.status != 'CE' and self.status != 'IE' and self.status != 'WJ' and self.status != 'WR':
+            if (
+                self.status != "CE"
+                and self.status != "IE"
+                and self.status != "WJ"
+                and self.status != "WR"
+            ):
                 time_consumption_str: str = table_data_list[7].get_text()
-                self.time_consumption = int(time_consumption_str.split(' ')[0])
+                self.time_consumption = int(time_consumption_str.split(" ")[0])
 
                 memory_consumption_str: str = table_data_list[8].get_text()
-                self.memory_consumption = int(memory_consumption_str.split(' ')[0])
+                self.memory_consumption = int(memory_consumption_str.split(" ")[0])
             else:
                 self.time_consumption = -1
                 self.memory_consumption = -1
 
         def __repr__(self) -> str:
-            return ('<Submission '
-                    f'time={self.time}, user={self.user_name}, contest={self.contest}, task={self.task}, '
-                    f'lang={self.lang_name}, lang_id={self.lang_id}, score={self.score}, magnification={self.magnification}, '
-                    f'submission_id={self.submission_id}, source_length={self.source_length}, status={self.status}, '
-                    f'time_consumption={self.time_consumption}, memory_consumption={self.memory_consumption}>')
+            return (
+                "<Submission "
+                f"time={self.time}, user={self.user_name}, contest={self.contest}, task={self.task}, "
+                f"lang={self.lang_name}, lang_id={self.lang_id}, score={self.score}, magnification={self.magnification}, "
+                f"submission_id={self.submission_id}, source_length={self.source_length}, status={self.status}, "
+                f"time_consumption={self.time_consumption}, memory_consumption={self.memory_consumption}>"
+            )
 
         def __str_to_status(self, status_str: str) -> SubmissionStatus:
-            if status_str == 'AC':
-                return 'AC'
-            if status_str == 'WA':
-                return 'WA'
-            if status_str == 'IE':
-                return 'IE'
-            if status_str == 'OLE':
-                return 'OLE'
-            if status_str == 'RE':
-                return 'RE'
-            if status_str == 'TLE':
-                return 'TLE'
-            if status_str == 'MLE':
-                return 'MLE'
-            if status_str == 'CE':
-                return 'CE'
-            if status_str == 'WJ':
-                return 'WJ'
-            if status_str == 'WR':
-                return 'WR'
-            msg: str = f'Invalid submission status: {status_str}'
+            if status_str == "AC":
+                return "AC"
+            if status_str == "WA":
+                return "WA"
+            if status_str == "IE":
+                return "IE"
+            if status_str == "OLE":
+                return "OLE"
+            if status_str == "RE":
+                return "RE"
+            if status_str == "TLE":
+                return "TLE"
+            if status_str == "MLE":
+                return "MLE"
+            if status_str == "CE":
+                return "CE"
+            if status_str == "WJ":
+                return "WJ"
+            if status_str == "WR":
+                return "WR"
+            msg: str = f"Invalid submission status: {status_str}"
             raise Exception(msg)
 
     pattern_starttime: Pattern[str] = re.compile(
-        r'var startTime = moment\("(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)\+09:00"\);')
+        r'var startTime = moment\("(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)\+09:00"\);'
+    )
     pattern_endtime: Pattern[str] = re.compile(
-        r'var endTime = moment\("(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)\+09:00"\);')
+        r'var endTime = moment\("(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)\+09:00"\);'
+    )
     contest_starttime: datetime
     contest_starttime_unix: int
     contest_endtime: datetime
@@ -153,11 +172,15 @@ class SubmissionListPage:
 
         # get submissions
         soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
-        table_rows: List[Tag] = soup.select('div.table-responsive table.table tbody tr')
-        self.submissions = [SubmissionListPage.Submission(table_row) for table_row in table_rows]
+        table_rows: List[Tag] = soup.select("div.table-responsive table.table tbody tr")
+        self.submissions = [
+            SubmissionListPage.Submission(table_row) for table_row in table_rows
+        ]
 
         # get contest title
-        contset_title_tag: Tag = soup.select('#navbar-collapse ul.nav.navbar-nav li a.contest-title')[0]
+        contset_title_tag: Tag = soup.select(
+            "#navbar-collapse ul.nav.navbar-nav li a.contest-title"
+        )[0]
         self.contest_title = contset_title_tag.get_text()
 
     def __extract_datetime(self, html: str, pattern: Pattern[str]) -> datetime:
@@ -172,6 +195,8 @@ class SubmissionListPage:
         return datetime(year, month, day, hour, minute, second)
 
     def __repr__(self) -> str:
-        return (f'<SubmissionListPage contest_title={self.contest_title} '
-                f'contest_starttime={self.contest_starttime} contest_endtime={self.contest_endtime} '
-                f'submissions={self.submissions}>')
+        return (
+            f"<SubmissionListPage contest_title={self.contest_title} "
+            f"contest_starttime={self.contest_starttime} contest_endtime={self.contest_endtime} "
+            f"submissions={self.submissions}>"
+        )
